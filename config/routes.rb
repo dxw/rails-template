dbc14a6e943f7f9baa3804a0d5946b0ad5c4879c
Stuart Harrison <pezholio@gmail.com>
2019-08-16 15:09:28 +0100

    Use standard.rb
    This may be a controversial choice, but:
    
    * It provides a series of sensible defaults, but is less opinionated than the defaults that ship with Rubocop
    * It favours clarity over individual style
    * Saves time bikeshedding over code style
    * It uses Rubocop under the hood, so can be extended

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  root to: "visitors#index"
 end
--- spec/features/visitors/home_page_spec.rb
+++ spec/features/visitors/home_page_spec.rb
@@ -4,13 +4,13 @@
 #   As a visitor
 #   I want to visit a home page
 #   So I can learn more about the website
-feature 'Home page' do
+feature "Home page" do
   # Scenario: Visit the home page
   #   Given I am a visitor
   #   When I visit the home page
   #   Then I see "Welcome"
-  scenario 'visit the home page' do
+  scenario "visit the home page" do
     visit root_path
-    expect(page).to have_content 'Welcome'
+    expect(page).to have_content "Welcome"
   end
 end
