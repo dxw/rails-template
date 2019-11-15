02bfeef063d8b490991e255c4b0b7ed2d06c0015
Tom Hipkin <tomhipkin@gmail.com>
2019-11-15 14:54:31 +0000

    Refactor database set up with new Rails 6 method
    * `rake db:prepare` looks to be equivalant. It runs the migration, if no database can be found it then seeks to create one. https://github.com/rails/rails/pull/35768
    * Given that this method is now tested as part of rails we have more confidence in the previous bug that reset the database despite it existing. Presumably this was caused by an error in how we were checking if a migration was pending so we should try removing our production guard.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,22 +5,10 @@ echo "ENTRYPOINT: Starting docker entrypoint…"
 
 setup_database()
 {
-  echo "ENTRYPOINT: Checking database setup is up-to-date…"
-  # Rails will throw an error if no database exists"
-  #   PG::ConnectionBad: FATAL:  database "roda-development" does not exist
-  if rake db:migrate:status &> /dev/null; then
-    echo "ENTRYPOINT: Database found, running db:migrate…"
-    rake db:migrate
-  else
-    if [ "$RAILS_ENV" == "production" ]; then
-      echo "ENTRYPOINT: Environment is production, doing nothing."
-    else
-      echo "ENTRYPOINT: Environment is in non-production, attempting to automatically create the database…"
-      echo "ENTRYPOINT: Running db:create db:schema:load…"
-      rake db:create db:environment:set db:schema:load
-    fi
-  fi
-  echo "ENTRYPOINT: Finished database setup"
+  echo "ENTRYPOINT: Running rake db:prepare…"
+  # Migrate the database and if one doesn't exist then create one
+  rake db:prepare
+  echo "ENTRYPOINT: Finished database setup."
 }
 
 if [ -z ${DATABASE_URL+x} ]; then echo "Skipping database setup"; else setup_database; fi
