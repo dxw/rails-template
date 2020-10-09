69b69d96412fc6d29c8fc99febdb19ab1b552fb2
Tom Hipkin <tomhipkin@gmail.com>
2020-10-09 11:30:07 +0100

    Remove default Rails scaffolded files from test coverage
    * These files all appear as 0% test coverage in Simplecov
    * We didn't write these files and can trust the Rails scaffold process somewhat to set these classes up correctly
    * We don't want to remove these files simply for the sake of code coverage as having their scaffolding there for apps that wish to use these Rails features may be useful.

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+# TODO: Remove nocov block if this scaffold is extended upon
+# :nocov:
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
   end
 end
+# :nocov:
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# TODO: Remove nocov block if this scaffold is extended upon
+# :nocov:
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+# :nocov:
