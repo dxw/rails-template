8c92b8bdc36ce940b93651637a600e9c98f3de8d
Stuart Harrison <pezholio@gmail.com>
2021-02-19 10:38:25 +0000

    Redirect requests not from the canonical hostname

---
+++ spec/requests/domain_redirect_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "Canonical domain redirect", type: :request do
+  before(:all) do
+    ENV["CANONICAL_HOSTNAME"] = "http://example.com"
+    Rails.application.reload_routes!
+  end
+
+  after(:all) do
+    ENV["CANONICAL_HOSTNAME"] = ""
+    Rails.application.reload_routes!
+  end
+
+  it "redirects to the canonical domain" do
+    expect(get("http://example.org/claim/new")).to redirect_to("http://example.com/claim/new")
+    expect(response.status).to eq(301)
+  end
+
+  it "keeps the original path" do
+    expect(get("http://example.org/cookies")).to redirect_to("http://example.com/cookies")
+    expect(response.status).to eq(301)
+  end
+
+  it "keeps query strings in place" do
+    expect(get("http://example.org/cookies?foo=bar")).to redirect_to("http://example.com/cookies?foo=bar")
+    expect(response.status).to eq(301)
+  end
+end
