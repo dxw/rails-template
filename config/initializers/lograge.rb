c1510e8edddbc2806aded59e81cb28dfb6f1f98b
Tom Hipkin <tomhipkin@gmail.com>
2021-07-23 17:27:01 +0100

    Rails logs include timestamps in UTC

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
   config.lograge.enabled = true
   config.lograge.ignore_actions = ["ApplicationController#health_check"]
+  config.lograge.custom_options = lambda do |event|
+    {time: Time.now.utc}
+  end
 end
