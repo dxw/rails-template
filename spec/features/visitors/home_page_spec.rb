da6d5f66579062ca3bc81bfc305067f7519f9bac
Robert Lee-Cann <leeky@leeky.org.uk>
2021-09-30 17:12:33 +0100

    Use RSpec's recommended defaults
     - Disable monkey patching (added back in 2014!)
     - Persist test failures to `spec/examples.txt`, which makes failures
    repeatable with `--only-failures`
     - Report 10 slowest examples at the end of the test run

--- spec/features/visitors/home_page_spec.rb
+++ spec/features/visitors/home_page_spec.rb
@@ -4,7 +4,7 @@
 #   As a visitor
 #   I want to visit a home page
 #   So I can learn more about the website
-feature "Home page" do
+RSpec.feature "Home page" do
   # Scenario: Visit the home page
   #   Given I am a visitor
   #   When I visit the home page
