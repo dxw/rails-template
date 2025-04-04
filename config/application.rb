edad65ad48dd62376fc797a00a390414dbc3bfb3
Ynda Jas <yndajas@gmail.com>
2025-04-04 13:39:36 +0100

    Run Rails update task
    `bundle exec rails app:update`
    
    I've kept the spring directory loading in `bin/rails` and `bin/rake`.
    This seems to have come from the [rails_app_composer][] "extras" recipe,
    but the reason for including it isn't well documented
    
    config/application.rb: retained the generator config. This was from one
    of the initial setup commits, but it feels opinionated and intentional
    
    config/environments/*.rb: mostly just accepted changes where we hadn't
    customised them and moved customisations to the end where we had (to
    improve future diffs)
    
    config/environments/test.rb: kept the customisation of
    `assets.css_compressor`, `after_initialize`, and `cache_classes`, but we
    should check if these are still needed
    
    config/initializers/assets.rb: retained the loading of the
    `node_modules` path. We only have dev dependencies listed in
    package.json, but it's reasonably likely that apps using this template
    will include some non-dev Node modules. Retaining this will reduce
    friction when doing so and is unlikely to impact app startup times in a
    meaningful way
    
    config/initializers/content_security_policy.rb: left enabled per
    4ad4b8fc3dc34ae83b4af1e41746f5a844935966 but updated to the latest
    format
    
    config/puma.rb:
    - https://github.com/rails/rails/pull/52541
    -
      https://github.com/rails/rails/commit/471ab2347fc8941ead6d9f95ef32c6686d706924
    
    The .prettierignore file is updated to include new files in the public
    directory
    
    [rails_app_composer]: https://github.com/RailsApps/rails_apps_composer

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative "boot"
 
 require "rails/all"
@@ -24,6 +22,11 @@ module RailsTemplate
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
